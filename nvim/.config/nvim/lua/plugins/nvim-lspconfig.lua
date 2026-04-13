eb2e55cc8e4677dbf971249c08b36915a94d3709
Nick Andreakos <18707825+nikolisan@users.noreply.github.com>
2026-04-13 02:40:17 +0100

    updated lazyvim with json support

---
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,23 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      jsonls = {
+        -- lazy-load schemastore when needed
+        before_init = function(_, new_config)
+          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+          vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+        end,
+        settings = {
+          json = {
+            format = {
+              enable = true,
+            },
+            validate = { enable = true },
+          },
+        },
+      },
+    },
+  },
+}
